e7d86ab3973e5ef07082e0058f9844bd32f2be6c
Silver Chen <silverchen@users.noreply.github.com>
2020-03-31 05:32:54 +0800

    Bump com.facebook.shimmer to 0.5.0 (#25)
    * Bump com.facebook.shimmer to 0.5.0
    
    * Revert breaking prop name change
    
    Co-authored-by: Joel Arvidsson <joel@oblador.se>

--- README.md
+++ README.md
@@ -99,7 +99,7 @@ import Shimmer from 'react-native-shimmer';
 | **`duration`**          | The shimmering animation duration in milliseconds.                                     | `1000`  |
 | **`pauseDuration`**     | The time interval between shimmerings in milliseconds.                                 | `400`   |
 | **`animationOpacity`**  | The opacity of the content while it is shimmering.                                     | `1`     |
-| **`opacity`**           | The opacity of the content before it is shimmering. _iOS only_                         | `0.5`   |
+| **`opacity`**           | The opacity of the content before it is shimmering.                                    | `0.5`   |
 | **`highlightLength`**   | The highlight length of shimmering. Range of 0–1. _iOS only_                           | `1`     |
 | **`beginFadeDuration`** | The duration of the fade used when shimmer begins. _iOS only_                          | `0`     |
 | **`endFadeDuration`**   | The duration of the fade used when shimmer ends. _iOS only_                            | `0`     |
--- android/src/main/java/com/oblador/shimmer/RNShimmeringView.java
+++ android/src/main/java/com/oblador/shimmer/RNShimmeringView.java
@@ -1,11 +1,16 @@
 package com.oblador.shimmer;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+
+import com.facebook.shimmer.Shimmer;
 import com.facebook.shimmer.ShimmerFrameLayout;
 
 
 public class RNShimmeringView extends ShimmerFrameLayout {
+    private Shimmer.Builder builder = new Shimmer.AlphaHighlightBuilder();
 
     public RNShimmeringView(Context context) {
         super(context);
@@ -15,41 +20,21 @@ public class RNShimmeringView extends ShimmerFrameLayout {
         super(context, attrs);
     }
 
-    public RNShimmeringView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public RNShimmeringView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
     }
 
-    private float mSpeed = 0.0f; // The speed of shimmering, in points per second.
-
-    public float getSpeed() {
-        return mSpeed;
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public RNShimmeringView(
+        Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
-    public void setSpeed(float speed) {
-        mSpeed = speed;
-        this.setDuration(this.getDuration());
+    public Shimmer.Builder getBuilder() {
+        return builder;
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        if (mSpeed > 0) {
-            int length = 0;
-            switch (this.getAngle()) {
-                case CW_90:
-                case CW_270:
-                    length = bottom - top;
-                    break;
-                case CW_0:
-                case CW_180:
-                default:
-                    length = right - left;
-                    break;
-            }
-            int duration = (int)(1000 * length / mSpeed);
-            if (duration != this.getDuration()) {
-                this.setDuration(duration);
-            }
-        }
-        super.onLayout(changed, left, top, right, bottom);
+    public void updateShimmer() {
+        setShimmer(builder.build());
     }
 }
