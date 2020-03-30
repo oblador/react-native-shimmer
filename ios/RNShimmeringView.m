5075c394e6fe7b0e11287e47014abb93a3b549d5
Joel Arvidsson <joel@oblador.se>
2020-03-31 00:02:28 +0200

    Unify indentation in iOS source code

--- ios/RNShimmeringView.m
+++ ios/RNShimmeringView.m
@@ -10,7 +10,8 @@
 #import <React/RCTAssert.h>
 #import "FBShimmeringLayer.h"
 
-@implementation RNShimmeringView {
+@implementation RNShimmeringView
+{
   CFTimeInterval _shimmeringDuration;
 }
 
@@ -32,40 +33,44 @@
   return self.contentView ? @[self.contentView] : @[];
 }
 
-- (void)layoutSubviews {
-    [super layoutSubviews];
+- (void)layoutSubviews
+{
+  [super layoutSubviews];
+  [self tryCorrectSpeedFromDuration];
+}
+
+- (CFTimeInterval)getShimmeringDuration
+{
+  return _shimmeringDuration;
+}
+
+- (void)setShimmeringDuration:(CFTimeInterval)shimmeringDuration
+{
+  if (shimmeringDuration < 0) {
+    shimmeringDuration = 0;
+  }
+  if (_shimmeringDuration != shimmeringDuration) {
+    _shimmeringDuration = shimmeringDuration;
     [self tryCorrectSpeedFromDuration];
+  }
 }
 
-- (CFTimeInterval)getShimmeringDuration {
-    return _shimmeringDuration;
-}
-
-- (void)setShimmeringDuration:(CFTimeInterval)shimmeringDuration {
-    if (shimmeringDuration < 0) {
-        shimmeringDuration = 0;
+- (void)tryCorrectSpeedFromDuration
+{
+  if (_shimmeringDuration > 0) {
+    FBShimmerDirection direction = self.shimmeringDirection;
+    FBShimmeringLayer *layer = (FBShimmeringLayer *)self.layer;
+    CGRect bounds = layer.contentLayer.bounds;
+    
+    CGFloat length = (direction == FBShimmerDirectionDown || direction == FBShimmerDirectionUp) ? CGRectGetHeight(bounds) : CGRectGetWidth(bounds);
+    
+    CGFloat speed = 230;
+    if (length > 0) {
+      speed = length / _shimmeringDuration;
     }
-    if (_shimmeringDuration != shimmeringDuration) {
-        _shimmeringDuration = shimmeringDuration;
-        [self tryCorrectSpeedFromDuration];
-    }
-}
-
-- (void)tryCorrectSpeedFromDuration {
-    if (_shimmeringDuration > 0) {
-        FBShimmerDirection direction = self.shimmeringDirection;
-        FBShimmeringLayer *layer = (FBShimmeringLayer *)self.layer;
-        CGRect bounds = layer.contentLayer.bounds;
-        
-        CGFloat length = (direction == FBShimmerDirectionDown || direction == FBShimmerDirectionUp) ? CGRectGetHeight(bounds) : CGRectGetWidth(bounds);
-        
-        CGFloat speed = 230;
-        if (length > 0) {
-            speed = length / _shimmeringDuration;
-        }
-        
-        [self setShimmeringSpeed:speed];
-    }
+    
+    [self setShimmeringSpeed:speed];
+  }
 }
 
 @end
