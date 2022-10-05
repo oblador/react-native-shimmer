f1c379a3f33627ec255474c3b4a1730bdfbb95c3
Joel Arvidsson <joel@oblador.se>
2022-10-05 17:49:19 +0200

    Upgrade Example app to react-native 0.70 (#38)
    * Upgrade Example app to react-native 0.70
    
    * Add switch to toggle animation in example app

--- Example/app.js
+++ Example/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Switch } from 'react-native';
 
 import Shimmer from 'react-native-shimmer';
 import logoSource from './react-logo.png';
@@ -16,12 +16,21 @@ const styles = StyleSheet.create({
     fontWeight: '300',
     marginBottom: 20,
   },
+  switch: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  switchLabel: {
+    fontSize: 16,
+    marginLeft: 10,
+  },
   loading: {
     marginVertical: 10,
   },
   loadingText: {
     textAlign: 'center',
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: '500',
   },
   reactLogo: {
@@ -31,14 +40,20 @@ const styles = StyleSheet.create({
 });
 
 export default function Example(props) {
+  const [animating, setAnimating] = React.useState(true);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Shimmer Example</Text>
-      <View>
-        <Shimmer style={styles.loading} duration={1000}>
+      <View style={styles.switch}>
+        <Switch value={animating} onValueChange={setAnimating} />
+        <Text style={styles.switchLabel}>Animating</Text>
+      </View>
+      <View key={animating ? '1' : '0'}>
+        <Shimmer style={styles.loading} duration={1000} animating={animating}>
           <Text style={styles.loadingText}>Loading...</Text>
         </Shimmer>
-        <Shimmer direction="down">
+        <Shimmer direction="down" animating={animating}>
           <Image source={logoSource} style={styles.reactLogo} />
         </Shimmer>
       </View>
